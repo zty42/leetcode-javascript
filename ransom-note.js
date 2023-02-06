5762eb03c8f0414befbfdb0557e712cf87d9aeb8
zhangtianyuan <tianyuan233.zhang@gmail.com>
2023-02-06 09:37:24 +0800

    canConstruct

---
+++ ransom-note.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function (ransomNote, magazine) {
+  const magazineArr = magazine.split('');
+  for (let i = 0; i < ransomNote.length; i++) {
+    const element = ransomNote[i];
+    if (magazineArr.includes(element)) {
+      const index = magazineArr.findIndex(_i => _i === element);
+      magazineArr.splice(index, 1);
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+console.log(canConstruct("aa", "aab"));
