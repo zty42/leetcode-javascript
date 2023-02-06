bd23a39d306f25443ae8e1d8b4413c730f5b5b44
zhangtianyuan <tianyuan233.zhang@gmail.com>
2023-02-06 15:01:22 +0800

    isPalindrome

---
+++ palindrome-number.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+// var isPalindrome = function (x) {
+//   if (x < 0) return false;
+
+//   return x.toString().split("").reverse().join("") === x.toString();
+// };
+
+// var isPalindrome = function (x) {
+//   if (x < 0) return false;
+
+//   const arr = x.toString().split("");
+//   for (let i = 0; i < array.length / 2; i++) {
+//     if (arr[i] !== arr[arr.length-1-i]) {
+//       return false
+//     }
+//   }
+//   return true
+// };
