5d8301ff2f3365717848277832dab403ab6056b4
zhangtianyuan <tianyuan233.zhang@gmail.com>
2023-02-10 17:29:16 +0800

    addToArrayForm

---
+++ add-to-array-form-of-integer.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} num
+ * @param {number} k
+ * @return {number[]}
+ */
+var addToArrayForm = function(num, k) {
+  const sum = BigInt(num.join()) + BigInt(k)
+  return sum.toString().split()
+    
+};
